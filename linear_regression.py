1726d263ad357cda7507f99978bafa1f1c89db2d
Ethan Farber <edf1247@gmail.com>
2024-05-02 12:15:17 -0400

    Added loss and backpropogation. TODO: Add iteration, add handling for all the data

--- linear_regression.py
+++ linear_regression.py
@@ -1,12 +1,34 @@
 import random
 petal_length = ["3", "2", "4", "3", "3.5", "2", "5.5", "1", "4.5"]
 petal_width = ["1.5", "1", "1.5", "1", ".5", ".5", "1", "1", "1"]
+training_iterations = 100
 
-def pred(x, y):
+
+def loss(guess, y):
+    cost = (1 / len(petal_length)) * ((y - guess) ** 2)
+    return cost
+
+def pred(x, w1, w2):
+    prediction = w1 * x + w2
+    return prediction
+
+def gradient_descent(learning_rate, w1, w2, cost):
+    w1 = w1 - (learning_rate * cost)
+    w2 = w2 - (learning_rate * cost)
+    return w1, w2
+
+def initialize_weights():
     slope = random.random()
     intercept = random.random()
-    initial_guess = slope * x + intercept
-    #mse = (1 / len(petal_length)) * (true_value - prediction)
-    return initial_guess
-prediction = pred(1,2)
-print(prediction)
+    return slope, intercept
+
+slope, intercept = initialize_weights()
+print(f"w1: {slope}\n w2: {intercept}")
+test_pred = pred(3, slope, intercept)
+print(f"Initial prediction: {test_pred}")
+cost = loss(test_pred, 1.5)
+print(f"Cost: {cost}")
+w1, w2 = gradient_descent(20, slope, intercept, cost=cost)
+print(f"New w1: {w1}\n New w2: {w2}")
+new_pred = pred(3, w1, w2)
+print(f"New pred: {new_pred}")
